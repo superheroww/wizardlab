319602c663fc664cd5568ed099a633af910ce7c4
Renato <galluzzi.renato@gmail.com>
2025-12-01 10:41:26 -0500

    1

---
+++ wizardlab/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./app/**/*.{ts,tsx,js,jsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
